82fac4ed38b14e2483a7497ac16290bcc93471a4
FireLord <firelord.xda@gmail.com>
2024-07-09 13:02:26 +0530

    first commit

---
+++ CapThat/presentation/CapThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapThatApp.swift
+//  CapThat
+//
+//  Created by Aman Kumar on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CapThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
